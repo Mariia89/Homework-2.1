3357245149ee2353170c8226dadf71828e6ba7d3
m.chudakova <masterova89@mail.ru>
2022-06-18 16:26:45 +0300

    Initial Commit

---
+++ Shared/Homework_2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_2_1App.swift
+//  Shared
+//
+//  Created by Мария Чудакова on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework_2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
