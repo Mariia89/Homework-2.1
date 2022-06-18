f6d6fa3f2d75ec7077f7a854be07c125372ea0f6
m.chudakova <masterova89@mail.ru>
2022-06-18 18:25:25 +0300

    setup project

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    static var previews: some View {      
+           ContentView()
+        
     }
 }
